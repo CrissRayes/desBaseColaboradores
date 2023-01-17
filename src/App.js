b79490483546ad17b6ba07b5eac10194dde36711
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-01-17 12:18:48 -0300

    muestra colaboradores

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useState } from 'react';
+import { BaseColaboradores } from './BaseColaboradores';
 
 function App () {
+  const [colaboradores, setColaboradores] = useState( BaseColaboradores )
+
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <ul>
+        { colaboradores.map( colaborador => (
+          <li key={ colaborador.id }>
+            <p>{ colaborador.nombre } - { colaborador.correo }</p>
+          </li>
+        ) ) }
+      </ul>
     </div>
   );
 }
